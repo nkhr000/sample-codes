a3281e59bf10bec75231b3d01062fc6ba67a735e
nkhr <2681080+nkhr000@users.noreply.github.com>
2022-07-10 14:53:58 +0900

    modified

--- python/boto3/firehouse/firehouse-put-data.py
+++ python/boto3/firehouse/firehouse-put-data.py
@@ -12,10 +12,14 @@ def put_records(stream, count):
     for num in range(count):
         event = {}
         keylist = ['A1', 'A2', 'B1', 'B2', 'B3', 'C1']
+        datalist = ['JP', 'UK', 'EU']
         event['key_name'] = random.choice(keylist)
         event['value'] = round(random.uniform(1.0, 200.0), 2)
         event['status'] = random.choice(['deleted', 'processing', 'done'])
-        event['number'] = num
+        event['number'] = {}
+        event['number']['type'] = 'IntegerType'
+        event['number']['value'] = num
+        event['list'] = datalist
         now = time.time()
         event[u'event_time'] = datetime.utcfromtimestamp(now).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
         event_record = json.dumps(event)
